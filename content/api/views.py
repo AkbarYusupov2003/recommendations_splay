8e3700b67d0275c72759b7183e595317729474b0
AkbarYusupov2003 <akbarbek.yusupov@gmail.com>
2023-12-27 18:04:18 +0500

    update

--- content/api/views.py
+++ content/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, filters, pagination, status, response
 from rest_framework.views import APIView
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Q
+from elasticsearch_dsl import Q as dsl_Q
 
 from content import models
 from content import documents
@@ -16,41 +16,49 @@ class RecommendationsForDetailAPIView(generics.GenericAPIView):
     pagination_class = pagination.LimitOffsetPagination
 
     def get_queryset(self):
-        # TODO Recommendations from: sponsors, title, genres fields
         age = 18  # self.request.auth.payload.get("age", 18)
         c_code = "UZ"  # self.request.auth.payload.get("c_code", "ALL")
 
-        content = get_object_or_404(models.Content, pk=self.kwargs["content_id"])
-        client = Elasticsearch(settings.ELASTICSEARCH_URL)
-        sponsors_ids = list(content.sponsors.all().values_list("pk", flat=True))
-        sponsors_query = "^1 ".join(str(x) for x in sponsors_ids)
-        document = self.document.search().filter("match", allowed_countries__country_code=c_code)
-        document = document.filter({"range": {"age_restrictions": {"lte": age}}})#.extra(size=100)
-
-        # response = document.query({
-        #     "query_string": {
-        #         f"query": f"sponsors.id:({sponsors_query})",
-        #         "rewrite": "scoring_boolean"
-        #     }
-        # })
-        print("Sponsors: ", sponsors_ids)
-        # Strict title check -> add from sponsors -> add from genres
-
-        res = client.search(
-            index="contents",
-            body={
-                "query": {
-                    "query_string": {
-                        f"query": f"sponsors.id:({sponsors_query})",
-                        "rewrite": "scoring_boolean"
-                    },
-                }
-            }
+        content = get_object_or_404(
+            models.Content, pk=self.kwargs["content_id"], age_restrictions__gte=age, allowed_countries__country_code=c_code
         )
-        for hit in res['hits']['hits']:
-            score = hit["_score"]
-            source = hit["_source"]
-            print(score, f"{source['id']}.{source['title_ru']}", source["sponsors"])
+        document = self.document.search().filter("match", allowed_countries__country_code=c_code)
+        document = document.filter({"range": {"age_restrictions": {"lte": age}}}) # .extra(size=1000)
+        # TODO Strict title check -> add from sponsors -> add from genres
+        # TODO Try to find with current title -> if not found split title by words -> Try to find ->
+
+        # Filtering by title
+        document = document.filter(
+            dsl_Q({
+                "match": {"title_ru.strict_edge": {"query": content.title_ru, "fuzziness": "0"}}
+            })
+        )
+        result = []
+        for x in document:
+            result.append(x)
+        # Filtering by sponsors
+        query = "^1 ".join(str(x) for x in list(content.sponsors.all().values_list("pk", flat=True)))
+        document = document.query({
+            "query_string": {
+                "query": f"sponsors.id:({query})",
+                "rewrite": "scoring_boolean"
+            }
+        })
+        for x in document:
+            if x not in result:
+                result.append(x)
+        # Filtering by genres
+        if len(result) < 30:
+            query = "^1 ".join(str(x) for x in list(content.genres.all().values_list("pk", flat=True)))
+            document = document.query({
+                "query_string": {
+                    "query": f"genres.id:({query}",
+                    "rewrite": "scoring_boolean"
+                }
+            })
+            for x in document:
+                if x not in result:
+                    result.append(x)
 
         return models.Content.objects.all()
 
--- content/documents.py
+++ content/documents.py
@@ -13,11 +13,7 @@ class ContentDocument(Document):
         fields={
             "multiple_words_ngram": fields.TextField(analyzer=analyzers.multiple_words_analyzer),
             "strict_edge": fields.TextField(analyzer=analyzers.strict_edge_ngram_analyzer),
-            "medium_edge": fields.TextField(analyzer=analyzers.medium_edge_ngram_analyzer),
-            "soft_edge": fields.TextField(analyzer=analyzers.soft_edge_ngram_analyzer),
-            "very_soft_edge": fields.TextField(analyzer=analyzers.very_soft_edge_ngram_analyzer),
             "strict_ngram": fields.TextField(analyzer=analyzers.strict_ngram_analyzer),
-            "medium_ngram": fields.TextField(analyzer=analyzers.medium_ngram_analyzer),
         }
     )
     title_uz = fields.TextField(
@@ -25,11 +21,7 @@ class ContentDocument(Document):
         fields={
             "multiple_words_ngram": fields.TextField(analyzer=analyzers.multiple_words_analyzer),
             "strict_edge": fields.TextField(analyzer=analyzers.strict_edge_ngram_analyzer),
-            "medium_edge": fields.TextField(analyzer=analyzers.medium_edge_ngram_analyzer),
-            "soft_edge": fields.TextField(analyzer=analyzers.soft_edge_ngram_analyzer),
-            "very_soft_edge": fields.TextField(analyzer=analyzers.very_soft_edge_ngram_analyzer),
             "strict_ngram": fields.TextField(analyzer=analyzers.strict_ngram_analyzer),
-            "medium_ngram": fields.TextField(analyzer=analyzers.medium_ngram_analyzer),
         }
     )
     # -------------------------------------------------------------------------------------------
